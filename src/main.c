2a64ee47eb478d4359762af875e38f72de8ad607
Spekkio <maxifoo@gmail.com>
2014-01-08 14:59:39 +0100

    Include config.h and show some info

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <SDL/SDL.h>
 #include <ode/ode.h>
+#include <config.h>
 
 #include "mainloop.h"
 #include "gl.h"
@@ -11,16 +12,30 @@ char readParams(const int argc, char ** argv)
 {
   if(argc>1)
     {
+#ifdef HAVE_PRINTF
       if(!strncmp(argv[argc-1],"--help",6))
 	{
-	  printf("\nno help.\n");
+	  printf("\n\t--version\tdisplay version.\n");
+	  printf("\t--help   \tdisplay this help.\n");
+	  printf("\t--about  \tabout this program.\n");
+	  printf("\n");
 	  return 1;
 	} else
       if(!strncmp(argv[argc-1],"--version",9))
 	{
-	  printf("\nversion: %s.\n",version);
+	  printf("\n%s.\n",PACKAGE_STRING);
+	  printf("\n");
+	  return 1;
+	} else
+      if(!strncmp(argv[argc-1],"--about",7))
+	{
+	  printf("\nA simple program for testing some OpenGL in SDL\n");
+	  printf("\nReport bugs to: %s.\n",PACKAGE_BUGREPORT);
+	  printf("Also see http://www.spekkio.se/ for my Public PGP key.\n");
+	  printf("\n");
 	  return 1;
 	}
+#endif
     }
   return 0;
 }
