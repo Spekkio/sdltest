e85886aa7d3d71505f39fa812b6b44040bf795ed
Spekkio <maxifoo@gmail.com>
2014-01-08 18:39:32 +0100

    Documentation test with texinfo

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf COPYING INSTALL Makefile.in aclocal.m4 autom4te.cache/ config.h.in configure depcomp install-sh missing Makefile config.h config.log config.status main src/.deps/ src/.dirstamp stamp-h1 *~ \#*\# *.o
+rm -rf COPYING INSTALL Makefile.in aclocal.m4 autom4te.cache/ config.h.in configure depcomp install-sh missing Makefile config.h config.log config.status main src/.deps/ src/.dirstamp stamp-h1 *~ \#*\# *.o mdate-sh texinfo.tex
 
 find . -regex "^.*\.o$" -delete
 find . -regex "^.*\.~$" -delete
