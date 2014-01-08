b0e6defb24b42e0255dd75045fc21434c198b0cd
Spekkio <maxifoo@gmail.com>
2014-01-08 18:43:30 +0100

    better clean rules

--- clean.sh
+++ clean.sh
@@ -2,4 +2,4 @@
 rm -rf COPYING INSTALL Makefile.in aclocal.m4 autom4te.cache/ config.h.in configure depcomp install-sh missing Makefile config.h config.log config.status main src/.deps/ src/.dirstamp stamp-h1 *~ \#*\# *.o mdate-sh texinfo.tex
 
 find . -regex "^.*\.o$" -delete
-find . -regex "^.*\.~$" -delete
+find . -regex "^.*~$" -delete
